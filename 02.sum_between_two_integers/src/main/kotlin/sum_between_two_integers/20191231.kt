ffa718decc4c4ff3fc84cf3b5d0c243d1a20d466
sonAjin <sonajin11@gmail.com>
2019-12-31 10:17:57 +0900

    20191231

--- 02.sum_between_two_integers/src/main/kotlin/sum_between_two_integers/20191231.kt
+++ 02.sum_between_two_integers/src/main/kotlin/sum_between_two_integers/20191231.kt
@@ -1,2 +1,7 @@
 package sum_between_two_integers
 
+fun sumBetweenTwoInteger01(a: Int, b: Int): Long =
+        when {
+            a <= b -> (a.toLong()..b.toLong()).sum()
+            else -> (a.toLong() downTo b.toLong()).sum()
+        }
--- 02.sum_between_two_integers/src/test/kotlin/sum_between_two_integers/20191231.kt
+++ 02.sum_between_two_integers/src/test/kotlin/sum_between_two_integers/20191231.kt
@@ -1,4 +1,13 @@
 package sum_between_two_integers
 
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
 class `20191231` {
+    @Test
+    fun `sum between_two_integers`() {
+        Assertions.assertThat(sumBetweenTwoInteger01(3, 5)).isEqualTo(12)
+        Assertions.assertThat(sumBetweenTwoInteger01(3, 3)).isEqualTo(3)
+        Assertions.assertThat(sumBetweenTwoInteger01(5, 3)).isEqualTo(12)
+    }
 }
