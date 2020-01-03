44001383c6c972ccf2195c900676ab32f2153c5c
sonAjin <sonajin11@gmail.com>
2020-01-03 15:00:46 +0900

    Add Problem solved to use fold
    add problem solved to use fold

--- 02.sum_between_two_integers/src/main/kotlin/sum_between_two_integers/20200103.kt
+++ 02.sum_between_two_integers/src/main/kotlin/sum_between_two_integers/20200103.kt
@@ -9,3 +9,11 @@ fun sumBetweenTwoInteger03(a: Int, b: Int): Long {
     return (min.toLong()..max.toLong()).sum()
 
 }
+
+
+fun sumBetweenTwoInteger04(a: Int, b: Int): Long {
+    val min = Integer.min(a, b)
+    val max = Integer.max(a, b)
+
+    return (min.toLong()..max.toLong()).fold(0L) { sum, element -> sum + element }
+}
--- 02.sum_between_two_integers/src/test/kotlin/sum_between_two_integers/20200103.kt
+++ 02.sum_between_two_integers/src/test/kotlin/sum_between_two_integers/20200103.kt
@@ -11,4 +11,10 @@ class `20200103` {
         Assertions.assertThat(sumBetweenTwoInteger03(3, 3)).isEqualTo(3)
         Assertions.assertThat(sumBetweenTwoInteger03(5, 3)).isEqualTo(12)
     }
+    @Test
+    fun `sum between_two_integers to fold`() {
+        Assertions.assertThat(sumBetweenTwoInteger04(3, 5)).isEqualTo(12)
+        Assertions.assertThat(sumBetweenTwoInteger04(3, 3)).isEqualTo(3)
+        Assertions.assertThat(sumBetweenTwoInteger04(5, 3)).isEqualTo(12)
+    }
 }
