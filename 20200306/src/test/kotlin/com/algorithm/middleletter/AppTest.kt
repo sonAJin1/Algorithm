373a196f4ebf2734baa34cb33a208e646040c0d4
sonAjin <sonajin11@gmail.com>
2020-03-06 12:30:19 +0900

    Solve Problem
    solve problem

--- 20200306/src/test/kotlin/com/algorithm/middleletter/AppTest.kt
+++ 20200306/src/test/kotlin/com/algorithm/middleletter/AppTest.kt
@@ -1,9 +1,23 @@
 package com.algorithm.middleletter
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
-import kotlin.test.assertNotNull
 
+fun solution(s: String): String {
+    val middleIdx = s.length / 2
+
+    if (s.length % 2 != 0) {
+        return s[middleIdx].toString()
+    }
+
+    return s[(middleIdx - 0.5).toInt()] + s[(middleIdx + 0.5).toInt()].toString()
+}
 
 class AppTest {
 
+    @Test
+    fun `get middle letter test`() {
+        assertThat(solution("abcde")).isEqualTo("c")
+        assertThat(solution("qwer")).isEqualTo("we")
+    }
 }
