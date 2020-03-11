4d4bd65441dad7a11d214d87942bee1eca824312
sonAjin <sonajin11@gmail.com>
2020-03-12 08:33:55 +0900

    Solve Problem
    solve problem

--- 20200312/src/test/kotlin/com/algorithm/findkim/AppTest.kt
+++ 20200312/src/test/kotlin/com/algorithm/findkim/AppTest.kt
@@ -1,8 +1,15 @@
 package com.algorithm.findkim
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(seoul: Array<String>): String  = "김서방은 " + seoul.indexOf("Kim") + "에 있다"
 
+
+class AppTest {
+    @Test
+    fun `김서방 찾기 테스트`(){
+        assertThat(solution(arrayOf("Jane", "Kim"))).isEqualTo("김서방은 1에 있다")
+    }
 }
