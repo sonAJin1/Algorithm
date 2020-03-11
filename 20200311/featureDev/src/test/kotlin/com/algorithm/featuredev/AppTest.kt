0445c3867c558ddd5e22d0378f3ef31465b0af17
sonAjin <sonajin11@gmail.com>
2020-03-12 08:36:04 +0900

    Move Folder
    move folder

--- 20200311/featureDev/src/test/kotlin/com/algorithm/featuredev/AppTest.kt
+++ 20200311/featureDev/src/test/kotlin/com/algorithm/featuredev/AppTest.kt
@@ -1,8 +1,34 @@
 package com.algorithm.featuredev
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+    val progresses = progresses
+    val speeds = speeds
+    countCompleteDate(progresses,speeds)
+    return intArrayOf(2)
+}
 
+fun countCompleteDate(progresses: IntArray, speeds: IntArray) {
+
+
+
+    progresses.mapIndexed { index, progress ->
+        //        (100 - progress) - (speeds[index] * n) < 0
+//        speeds[index] * n < 100 - progress
+//        println((100 - progress).div(speeds[index]))
+
+
+        0 > (100 - progress) / speeds[index]
+
+    }
+}
+
+class AppTest {
+    @Test
+    fun `기능 개발 테스트`() {
+        assertThat(solution(intArrayOf(93, 30, 5), intArrayOf(1, 30, 5))).isEqualTo(intArrayOf(2, 1))
+    }
 }
