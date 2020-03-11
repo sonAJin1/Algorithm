90a9b674efe9b3d842ae17d523cb316f1fbccfbb
sonAjin <sonajin11@gmail.com>
2020-03-11 09:21:13 +0900

    Solve Problem
    solve problem

--- 20200311/src/test/kotlin/com/algorithm/descendingsort/AppTest.kt
+++ 20200311/src/test/kotlin/com/algorithm/descendingsort/AppTest.kt
@@ -1,8 +1,17 @@
 package com.algorithm.descendingsort
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution01(s: String): String = s.toCharArray().sortedWith(compareBy({ it }, { it.isLowerCase() })).sortedDescending().joinToString().replace(", ", "")
+fun solution02(s: String): String = s.toCharArray().sortedArrayDescending().joinToString("")
 
+
+class AppTest {
+    @Test
+    fun `내림차순 정렬`() {
+        assertThat(solution01("feIdwald")).isEqualTo("wlfeddaI")
+        assertThat(solution02("feIdwald")).isEqualTo("wlfeddaI")
+    }
 }
