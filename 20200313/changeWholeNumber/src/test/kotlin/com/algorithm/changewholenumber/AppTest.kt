038040f5d5f032625bbe78a9be6286a0ea28e9f9
손아진 <sonajin11@gmail.com>
2020-03-13 08:21:42 +0900

    Solve Problem
    solve problem

--- 20200313/changeWholeNumber/src/test/kotlin/com/algorithm/changewholenumber/AppTest.kt
+++ 20200313/changeWholeNumber/src/test/kotlin/com/algorithm/changewholenumber/AppTest.kt
@@ -1,8 +1,16 @@
 package com.algorithm.changewholenumber
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(s: String): Int = s.toInt()
 
+
+class AppTest {
+    @Test
+    fun `정수로 변환 테스트`() {
+        assertThat(solution("1234")).isEqualTo(1234)
+        assertThat(solution("-1234")).isEqualTo(-1234)
+    }
 }
