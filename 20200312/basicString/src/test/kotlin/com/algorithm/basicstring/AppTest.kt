16b1360f63081148ce5effc354310655344f134c
sonAjin <sonajin11@gmail.com>
2020-03-12 10:44:12 +0900

    Solve Problem
    solve problem

--- 20200312/basicString/src/test/kotlin/com/algorithm/basicstring/AppTest.kt
+++ 20200312/basicString/src/test/kotlin/com/algorithm/basicstring/AppTest.kt
@@ -1,8 +1,20 @@
 package com.algorithm.basicstring
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution01(s: String): Boolean =
+    when(s.length) {
+        4, 6 -> s.all { it in '0'..'9' }
+        else -> false
+    }
 
+
+class AppTest {
+    @Test
+    fun `기본 문자열 다루기 테스트`() {
+        assertThat(solution01("a234")).isEqualTo(false)
+        assertThat(solution01("1234")).isEqualTo(true)
+    }
 }
