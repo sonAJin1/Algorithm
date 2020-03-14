e77f83647d44a41967a57a5172b65a81bab00a65
sonAjin <sonajin11@gmail.com>
2020-03-14 16:06:09 +0900

    Solve Problem
    solve problem

--- 20200314/tower/src/test/kotlin/com/algorithm/tower/AppTest.kt
+++ 20200314/tower/src/test/kotlin/com/algorithm/tower/AppTest.kt
@@ -1,8 +1,26 @@
 package com.algorithm.tower
 
+import org.assertj.core.api.Assertions.assertThat
+import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
+
+fun solution1(heights: IntArray): IntArray {
+    val result = mutableListOf<Int>()
+    for (i in heights.size - 1 downTo 0) {
+        result.add(
+                0,
+                heights.dropLast(heights.size - i).indexOfLast { it > heights[i] } + 1
+        )
+    }
+    return result.toIntArray()
+}
+
 class AppTest {
+    @Test
+    fun `탑 높이 테스트`() {
+         assertThat(solution1(intArrayOf(6, 9, 5, 7, 4))).isEqualTo(intArrayOf(0, 0, 2, 2, 4))
+    }
 
 }
