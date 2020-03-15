837c3905a56760ba76aa0bd677923d8d4d811153
sonAjin <sonajin11@gmail.com>
2020-03-16 08:05:12 +0900

    Solve Problem
    solve problem

--- 20200316/src/test/kotlin/com/algorithm/hidenumber/AppTest.kt
+++ 20200316/src/test/kotlin/com/algorithm/hidenumber/AppTest.kt
@@ -1,8 +1,16 @@
 package com.algorithm.hidenumber
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(phone_number: String): String = phone_number.dropLast(4).map { "*" }.joinToString("") + phone_number.takeLast(4)
 
+
+
+class AppTest {
+    @Test
+    fun `문자열 숨기기 테스트`(){
+        assertThat(solution("01033334444")).isEqualTo("*******4444")
+    }
 }
