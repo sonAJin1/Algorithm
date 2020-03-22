e3ea841078382e6d5f3cad6000fee4a0dfa2275f
손아진 <sonajin11@gmail.com>
2020-03-23 08:34:29 +0900

    Solve Problem
    solve problem

--- 20200323/src/test/kotlin/com/algorithm/evenodd/AppTest.kt
+++ 20200323/src/test/kotlin/com/algorithm/evenodd/AppTest.kt
@@ -1,8 +1,21 @@
 package com.algorithm.evenodd
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(num: Int): String =
+        when (num % 2) {
+            0 -> "Even"
+            else -> "Odd"
+        }
 
+
+class AppTest {
+    @Test
+    fun `even and odd`() {
+        assertThat(solution(3)).isEqualTo("Odd")
+        assertThat(solution(4)).isEqualTo("Even")
+        assertThat(solution(0)).isEqualTo("Even")
+    }
 }
