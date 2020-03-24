6e11b2f06bf18f398fe37c875bde3830bec03e3b
손아진 <sonajin11@gmail.com>
2020-03-25 08:06:14 +0900

    Solve Problem
    solve problem

--- 20200325/src/test/kotlin/com/algorithm/factor/AppTest.kt
+++ 20200325/src/test/kotlin/com/algorithm/factor/AppTest.kt
@@ -1,8 +1,25 @@
 package com.algorithm.factor
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(n: Int): Int {
+    return findingFactor(n).sum()
+}
 
+fun findingFactor(n: Int): List<Int> {
+    val factor = ArrayList<Int>()
+    (1..n).forEach {
+        if (n % it == 0) factor.add(it)
+    }
+    return factor
+}
+
+class AppTest {
+    @Test
+    fun `sum of factor`() {
+        assertThat(findingFactor(12)).isEqualTo(listOf(1, 2, 3, 4, 6, 12))
+        assertThat(solution(12)).isEqualTo(28)
+    }
 }
