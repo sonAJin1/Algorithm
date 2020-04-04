3bba1892ead89f9defb73435bbfbbbc8c0e96b95
sonAjin <sonajin11@gmail.com>
2020-04-04 13:30:38 +0900

    Init Project
    init project

---
+++ test02/src/test/kotlin/com/algorithm/test02/AppTest02.kt
@@ -0,0 +1,17 @@
+package com.algorithm.test02
+
+import org.assertj.core.api.Assertions.assertThat
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+fun solution02(){
+
+}
+
+
+class AppTest02 {
+    @Test
+    fun `test`(){
+        assertThat(solution02())
+    }
+}
