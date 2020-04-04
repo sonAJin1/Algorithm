77b3b6bcf411bc961c2bd12032f9b038640e5b9f
sonAjin <sonajin11@gmail.com>
2020-04-04 13:12:08 +0900

    Solve Problem
    solve problem

--- 20200404/src/test/kotlin/com/algorithm/average/AppTest.kt
+++ 20200404/src/test/kotlin/com/algorithm/average/AppTest.kt
@@ -1,8 +1,19 @@
 package com.algorithm.average
 
+import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
-class AppTest {
+fun solution(arr: IntArray): Double = arr.fold(0, { total, next -> total + next }) / arr.size.toDouble()
+fun solution02(arr: IntArray): Double = arr.average()
 
+
+class AppTest {
+    @Test
+    fun `find average`() {
+        assertThat(solution(intArrayOf(1, 2, 3, 4))).isEqualTo(2.5)
+        assertThat(solution02(intArrayOf(1, 2, 3, 4))).isEqualTo(2.5)
+        assertThat(solution(intArrayOf(5, 5))).isEqualTo(5.0)
+        assertThat(solution02(intArrayOf(5, 5))).isEqualTo(5.0)
+    }
 }
